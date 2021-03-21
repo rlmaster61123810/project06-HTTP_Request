5bee4cc18b737fadabcec39f81b42f112d42c011
Nontawat Wuttifaey <mistidea9@gmail.com>
2021-03-21 09:42:04 +0700

    http

---
+++ http_request/lib/screen/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+
+
+
+
+
+
+class _HomeScreenState extends State<HomeScreen> {
+
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title: Text(
+           "Home Screen"
+         ),
+       ),
+       body: Container(
+         child: Center(
+           
+         ),
+       ),
+    );
+  }
+}
