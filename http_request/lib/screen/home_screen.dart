c8a9c66098ede84cf0ca318b994f4d90983e78e2
Nontawat Wuttifaey <mistidea9@gmail.com>
2021-03-21 11:17:42 +0700

    todo

--- http_request/lib/screen/home_screen.dart
+++ http_request/lib/screen/home_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_request/model/todo.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -8,32 +11,51 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-
-
-
-
-
-
 class _HomeScreenState extends State<HomeScreen> {
+List<Todo> todos = [];
 
+  Future fetchTodo() async {
+    http.Response request = await http.get(
+      Uri.parse(
+        "https://jsonplaceholder.typicode.com/todos",
+      ),
+    );
+    print(request);   
+    setState(() {
+          todos = (jsonDecode(request.body) as List)
+          .map(
+            (json)=> Todo. fromJson(json) , 
+          )
+          .toList();
+        });
+  }
 
-
-
-
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         title: Text(
-           "Home Screen"
-         ),
-       ),
-       body: Container(
-         child: Center(
-           
-         ),
-       ),
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Container( 
+      child: ListView(
+        children: todos
+        .map(
+          (item) => ListTile(
+            title: Text(
+              item.title,
+            ),
+            subtitle: item.completed ? Text("แล้วละนะ") : Text("ยังไม่แล้วนะ"),
+          ),
+          )
+          .toList(),
+       ) 
+      ),
     );
   }
 }
